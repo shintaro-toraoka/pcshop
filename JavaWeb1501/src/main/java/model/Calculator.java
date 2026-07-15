603693a1de080f00354e49cb886180287ad7dee5
PCN-1620\cscuser1620 <toraoka-s@cscnet.co.jp>
2026-07-15 16:58:32 +0900

    税額メソッド、税込みメソッドを作りました。

--- JavaWeb1501/src/main/java/model/Calculator.java
+++ JavaWeb1501/src/main/java/model/Calculator.java
@@ -1,41 +1,57 @@
 package model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * 税込み計算・カートに追加された商品分の税込み合計金額
  */
 public class Calculator {
 	//消費税10％
-	final double tax = 1.1;
+	private final static BigDecimal TAX_RATE = new BigDecimal("0.1");
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		List<Product> productList = new ArrayList<Product>();
 		//商品追加
 		productList.add(new Product("A110", "無線マウス", 2000));
-		productList.add(new Product("A120", "薄型キーボード", 3600));
-		
+		//productList.add(new Product("A120", "薄型キーボード", 3600));
+
 		//Calcuratorのcalcuratorメソッドを呼び出す
 		Calculator calculator = new Calculator();
 		int total = calculator.getTotalAmount(productList);
-		
+
 		//合計金額を出力する
 		System.out.println(total);
-		
+
+		System.out.println(calculator.tax(111));
 
 	}
+
 	/**
-	 * 税込み価格を算出
-	 * @param productPrice：税抜き商品価格
+	 * 税額のみ算出
+	 * @param productPrice：税抜商品価格
+	 * @return 
+	 */
+	//税抜価格×税率
+	public int tax(int productPrice) {
+		//int型 productPriceをBigDecimal型 priceにキャスト
+		BigDecimal price = BigDecimal.valueOf(productPrice);
+		return price.multiply(TAX_RATE).setScale(0, RoundingMode.DOWN).intValue();
+	}
+
+	/**
+	 * 税込価格を算出
+	 * @param productPrice：税抜商品価格
 	 * @return
 	 */
+
 	public int addTax(int productPrice) {
-		
 		int total = 0;
-		total = (int)(productPrice * tax);
+		total = productPrice + tax(productPrice);
 		return total;
-		
 	}
 
 	/**
@@ -50,8 +66,7 @@ public class Calculator {
 			amount += addTax(listProd.get(i).getPrice());
 		}
 		return amount;
-		
-	}
 
+	}
 
 }
