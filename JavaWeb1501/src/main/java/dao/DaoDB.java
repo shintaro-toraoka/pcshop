06acb5c87bf4cdf4eeabc8a3ebf7d0c14e6f7cd3
DESKTOP-NERQH65\kusa <mitsuru.yokota@gmail.com>
2026-07-28 23:21:36 +0900

    DB接続情報を外付けファイルに設定
    本来、Warファイルに梱包しない場所に保存するが、研修用なのでwarに梱包。
    接続先を変更する際は、db.properties.xmlを変更してTomcat再起動

--- JavaWeb1501/src/main/java/dao/DaoDB.java
+++ JavaWeb1501/src/main/java/dao/DaoDB.java
@@ -1,24 +1,74 @@
 package dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DaoDB {
 
-	private final String DBNAME = "cscdb";
-	private final String HOSTNAME = "localhost";
-	private final String PORTNO = "3306";
-	private final String DBUSER = "RyanGosling";
-	private final String DBPASSWD = "DBcsc26Newbie";
-	
-	// DB接続処理	
-	protected Connection getConnection() throws ClassNotFoundException, SQLException {
-		// MySQL JDBCドライバを読み込む	
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url = String.format("jdbc:mysql://%s:%s/%s?allowPublicKeyRetrieval=true&useSSL=false", HOSTNAME, PORTNO, DBNAME);
-		return DriverManager.getConnection(url, DBUSER, DBPASSWD);
-	}
-	
+	private static final String CONFIG_FILE = "/db.properties.xml";
 
+	private static final Properties DB_PROPERTIES = loadProperties();
+
+	/**
+	 * XMLファイルからDB接続情報を読み込みます。
+	 */
+	private static Properties loadProperties() {
+
+		Properties properties = new Properties();
+
+		try (InputStream inputStream =DaoDB.class.getResourceAsStream(CONFIG_FILE)) {
+
+			if (inputStream == null) {
+				throw new IllegalStateException("DB設定ファイルが見つかりません: " + CONFIG_FILE);
+			}
+
+			properties.loadFromXML(inputStream);
+
+			return properties;
+
+		} catch (IOException e) {
+			throw new IllegalStateException("DB設定ファイルの読み込みに失敗しました: " + CONFIG_FILE, e);
+		}
+	}
+
+	/**
+	 * DB接続を取得します。
+	 */
+	protected Connection getConnection()
+			throws ClassNotFoundException, SQLException {
+
+		String dbName =
+				DB_PROPERTIES.getProperty("db.name");
+
+		String hostname =
+				DB_PROPERTIES.getProperty("db.hostname");
+
+		String port =
+				DB_PROPERTIES.getProperty("db.port");
+
+		String dbUser =
+				DB_PROPERTIES.getProperty("db.user");
+
+		String dbPassword =
+				DB_PROPERTIES.getProperty("db.password");
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		String url = String.format(
+				"jdbc:mysql://%s:%s/%s"
+						+ "?allowPublicKeyRetrieval=true"
+						+ "&useSSL=false",
+				hostname,
+				port,
+				dbName);
+
+		return DriverManager.getConnection(
+				url,
+				dbUser,
+				dbPassword);
+	}
 }
