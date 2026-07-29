1f5c4d968d82b559691fd2da2c7e48bf7aee0f7d
M-YOKOTA\mitsu <mitsuru.yokota@gmail.com>
2026-07-30 08:42:20 +0900

    categoryがNullのものは無視するように修正
    せっかくなのだから使いましょう

--- JavaWeb1501/src/main/java/dao/ProductDaoDB.java
+++ JavaWeb1501/src/main/java/dao/ProductDaoDB.java
@@ -28,11 +28,11 @@ public class ProductDaoDB extends DaoDB implements ProductDao {
 			String sql;
 			if (keyword != null && !keyword.isEmpty()) {
 
-			    sql = "SELECT * FROM products WHERE product_name LIKE ?";
+			    sql = "SELECT * FROM products WHERE category IS NOT NULL AND product_name LIKE ?";
 
 			} else {
 
-			    sql = "SELECT * FROM products";
+			    sql = "SELECT * FROM products WHERE category IS NOT NULL";
 
 			}			
 			
